3e22d0f8c6759a8c88a1aaca4c3b91d7683eef79
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-19 14:57:21 +0200

    ripetuto scheletro per capire la base e come procedere

--- Program.cs
+++ Program.cs
@@ -2,17 +2,28 @@
 
 //Creare una classe Prodotto che gestisce i prodotti dello shop.
 
+
+
+Product firstProduct = new Product();
+
+firstProduct.code = 1;
+firstProduct.name = "test1";
+firstProduct.description = "lorem ipsum";
+firstProduct.price = 19.50;
+firstProduct.vat = 25;
+
+Console.WriteLine(firstProduct.code);
+Console.WriteLine(firstProduct.name);
+Console.WriteLine(firstProduct.description);
+Console.WriteLine(firstProduct.price);
+Console.WriteLine(firstProduct.vat);
+
 public class Product
 {
-    int code;
-    string name;
-    string description;
-    double price;
-    int vat;
-
-    public Product()
-    {
-        Random rnd = new Random();
-        code = rnd.Next();
-    }
+    public int code;
+    public string name;
+    public string description;
+    public double price;
+    public int vat;
 }
+
