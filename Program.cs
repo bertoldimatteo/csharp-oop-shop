ce4d4de15568518c5d4f7fbc856af4be38db895d
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-19 14:35:07 +0200

    inizio setting oggetto prodotto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//Creare una classe Prodotto che gestisce i prodotti dello shop.
+
+public class Product
+{
+    int code;
+    string name;
+    string description;
+    double price;
+    int vat;
+
+    public Product()
+    {
+        Random rnd = new Random();
+        code = rnd.Next();
+    }
+}
