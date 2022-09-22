cdf8837329a2e30e217b4def53c353559fcd0729
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-22 15:45:28 +0200

    implementazione classe Water

--- Product.cs
+++ Product.cs
@@ -1,12 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-public class Product
+class Product
 {
     public int code { get; set; }
     public string name { get; set; }
     public string description { get; set; }
     public double price { get; set; }
     public int vat { get; set; }
+
+    public Product(int code, string name, string description, double price, int vat)
+    {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vat = vat;
+    }
 
     public int GenerateCodeProduct()
     {
@@ -23,3 +32,21 @@ public class Product
     }
 }
 
+class Water: Product 
+{
+    public double liters { get; set; }
+    public int ph { get; }
+    public string source { get; }
+
+    public Water(int code, string name, string description, double price, int vat, double liters, int ph, string source): base(code, name, description, price, vat)
+    {
+        this.liters = liters;
+        this.ph = ph;
+        this.source = source;
+    }
+
+    public double drink(double litersToDrink)
+    {
+        return liters - litersToDrink;
+    }
+}
